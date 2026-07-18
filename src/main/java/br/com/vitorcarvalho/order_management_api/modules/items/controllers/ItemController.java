34239fef1ab455544e02e2b6b4d3f561e43b54eb
Vitor Carvalho <vitorclocked@gmail.com>
2026-07-18 12:16:02 -0300

    doc: adiciona documentação em ItemController

--- src/main/java/br/com/vitorcarvalho/order_management_api/modules/items/controllers/ItemController.java
+++ src/main/java/br/com/vitorcarvalho/order_management_api/modules/items/controllers/ItemController.java
@@ -13,12 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.vitorcarvalho.order_management_api.modules.items.ItemEntity;
 import br.com.vitorcarvalho.order_management_api.modules.items.repositories.ItemRepository;
 import br.com.vitorcarvalho.order_management_api.modules.items.useCases.ItemUseCase;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
 
 
 @RestController
 @RequestMapping("/items")
+@Tag(name = "Item", description = "Item's information")
 public class ItemController {
     private ItemRepository itemRepository;
     private ItemUseCase itemUseCase;
@@ -29,11 +32,13 @@ public class ItemController {
     }
 
     @PostMapping("/")
+    @Operation(summary = "Registration", description = "This function is responsible for recording the items.")
     public ItemEntity create(@Valid @RequestBody ItemEntity itemEntity) {
         return this.itemRepository.save(itemEntity);
     }
 
     @GetMapping("")
+    @Operation(summary = "Listing", description = "This funcion is responsible for listing the items based on a filter.")
     public ResponseEntity<List<ItemEntity>> list(@RequestParam(required = false) String name, @RequestParam(required = false) String category) {
         List<ItemEntity> items = itemUseCase.findByFilter(name, category);
         return ResponseEntity.ok(items);
