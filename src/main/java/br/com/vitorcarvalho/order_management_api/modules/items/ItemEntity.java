a58d649b071d08b2a57625bed8756b5995b2c211
Vitor Carvalho <vitorclocked@gmail.com>
2026-07-18 00:49:46 -0300

    feat: adiciona atributo de preço em ItemEntity

--- src/main/java/br/com/vitorcarvalho/order_management_api/modules/items/ItemEntity.java
+++ src/main/java/br/com/vitorcarvalho/order_management_api/modules/items/ItemEntity.java
@@ -1,15 +1,18 @@
 package br.com.vitorcarvalho.order_management_api.modules.items;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -25,6 +28,11 @@ public class ItemEntity {
     private String name;
 
     private String description;
+
+    @NotNull
+    @DecimalMin(value = "0.01", message = "The price has to be greater than zero.")
+    @Column(precision = 10, scale = 2, nullable = false)
+    private BigDecimal price;
 
     @NotNull
     private Integer quantity;
