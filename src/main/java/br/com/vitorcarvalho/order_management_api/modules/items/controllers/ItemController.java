458abcba17dbfbb4137b5ba2486f7511b3808a4d
Vitor Carvalho <vitorclocked@gmail.com>
2026-07-20 15:42:54 -0300

    docs: adiciona documentação dos métodos

--- src/main/java/br/com/vitorcarvalho/order_management_api/modules/items/controllers/ItemController.java
+++ src/main/java/br/com/vitorcarvalho/order_management_api/modules/items/controllers/ItemController.java
@@ -23,6 +23,8 @@ import br.com.vitorcarvalho.order_management_api.modules.items.dto.UpdateItemReq
 import br.com.vitorcarvalho.order_management_api.modules.items.repositories.ItemRepository;
 import br.com.vitorcarvalho.order_management_api.modules.items.useCases.ItemUseCase;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -35,15 +37,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RequestMapping("/items")
 @Tag(name = "Item", description = "Item's information")
 public class ItemController {
-    private ItemRepository itemRepository;
     private ItemUseCase itemUseCase;
 
-    ItemController(ItemRepository itemRepository, ItemUseCase itemUseCase){
-        this.itemRepository = itemRepository;
+    ItemController(ItemUseCase itemUseCase){
         this.itemUseCase = itemUseCase;
     }
 
     @PostMapping("/")
+    @ApiResponses({
+        @ApiResponse(responseCode = "201", description = "The item was created with success!"),
+        @ApiResponse(responseCode = "400", description = "Please verify the item's attributes.")
+    })
     @Operation(summary = "Registration", description = "This function is responsible for recording the items.")
     public ResponseEntity<ItemEntity> create(@Valid @RequestBody CreateItemRequest createdItem) {
         ItemEntity item = this.itemUseCase.create(createdItem);
@@ -52,6 +56,9 @@ public class ItemController {
 
     @GetMapping("")
     @Operation(summary = "List", description = "This funcion is responsible for listing the items based on a filter.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "The list was generated with success!")
+    })
     public ResponseEntity<List<ItemEntity>> list(@RequestParam(required = false) String name, @RequestParam(required = false) String category) {
         List<ItemEntity> items = itemUseCase.findByFilter(name, category);
         return ResponseEntity.ok(items);
@@ -59,6 +66,10 @@ public class ItemController {
     
     @PatchMapping("/{id}")
     @Operation(summary = "Patch", description = "This function is responsible for updating partially an item by ID.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "The item was patched with success!"),
+        @ApiResponse(responseCode = "400", description = "Please verify the ID provided.")
+    })
     public ResponseEntity<ItemEntity> patch(@PathVariable UUID id, @Valid @RequestBody PatchItemRequest patchedItemRequest) {
         ItemEntity patchedItem = this.itemUseCase.patch(id, patchedItemRequest);
         return ResponseEntity.ok(patchedItem);
@@ -66,6 +77,10 @@ public class ItemController {
 
     @PutMapping("/{id}")
     @Operation(summary = "Update", description = "This funcion is responsible for updating an item by ID.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "The item was updated with success!"),
+        @ApiResponse(responseCode = "400", description = "Please verify the ID provided.")
+    })
     public ResponseEntity<ItemEntity> update(@PathVariable UUID id, @Valid @RequestBody UpdateItemRequest updatedItemRequest) {
         ItemEntity updatedItem = this.itemUseCase.update(id, updatedItemRequest);
         return ResponseEntity.ok(updatedItem);
@@ -73,6 +88,10 @@ public class ItemController {
 
     @DeleteMapping("/{id}")
     @Operation(summary = "Delete", description = "This function is responsible for deleting an item by ID.")
+    @ApiResponses({
+        @ApiResponse(responseCode = "200", description = "The item was deleted with success!"),
+        @ApiResponse(responseCode = "400", description = "Please verify the ID provided.")
+    })
     public void delete(@PathVariable UUID id){
         itemUseCase.delete(id);
     }
